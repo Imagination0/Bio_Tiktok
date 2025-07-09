271bd0b0f632969085f58b48a9a5d4b60fd4cf9c
Imagination0 <113436278+Imagination0@users.noreply.github.com>
2025-07-09 23:43:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
-import asyncio
-import schedule
 import time
+import schedule
 from get_views import get_total_views
 from update_bio import update_tiktok_bio
 
-USERNAME = 'your_username'
+USERNAME = 'your_username'  # Ganti dengan username TikTok kamu
 
-async def job():
+def job():
     total_views = get_total_views(USERNAME)
-    bio_text = f"Jumblah Total Views: {total_views}"
-    await update_tiktok_bio(bio_text)
+    bio_text = f"Total Views: {total_views}"
+    update_tiktok_bio(bio_text)
     print(f"[UPDATED] {bio_text}")
 
-def run_schedule():
-    schedule.every(20).minutes.do(lambda: asyncio.run(job()))
+if __name__ == "__main__":
+    schedule.every(20).minutes.do(job)
+    job()
     while True:
         schedule.run_pending()
         time.sleep(1)
-
-if __name__ == "__main__":
-    run_schedule()
