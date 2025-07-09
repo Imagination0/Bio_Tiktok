697fa9d0800222dbcaa11810bfabc3e95fea24f5
Imagination0 <113436278+Imagination0@users.noreply.github.com>
2025-07-09 23:47:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ USERNAME = 'your_username'  # Ganti dengan username TikTok kamu
 
 def job():
     total_views = get_total_views(USERNAME)
-    bio_text = f"Total Views: {total_views}"
+    bio_text = f"Total Views Seluruh Video : {total_views}"
     update_tiktok_bio(bio_text)
     print(f"[UPDATED] {bio_text}")
 
